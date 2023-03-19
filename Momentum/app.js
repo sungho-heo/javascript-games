35208aaecba4ce1f34f5390c6916793d24be47de
Sungho <hurgj123kr@gmail.com>
2023-03-19 12:16:11 +0900

    window wifi offline, online event add

--- Momentum/app.js
+++ Momentum/app.js
@@ -2,7 +2,7 @@ const title = document.querySelector("div.header h1")
 
 function handleTitleEvent() {
     const body = document.querySelector("body");
-    
+    // document.body.style.backgorundColor 이런식으로 가져오는게 가능한게 body다.. 신기하네
     if (body.style.backgroundColor === "green") {
         body.style.backgroundColor = "white";
     } else {
@@ -11,5 +11,12 @@ function handleTitleEvent() {
     
 
 };
-
+function handleWindowOffline() {
+    alert("oh no wifi plz connect")
+}
+function handleWindowOnline() {
+    alert("gob job wifi connect")
+}
 title.addEventListener("click", handleTitleEvent);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
