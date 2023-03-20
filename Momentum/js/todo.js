a029e5b7a1dfe2eca7ee847501bbbbdb8668bf24
Sungho <hurgj123kr@gmail.com>
2023-03-20 20:50:13 +0900

    todo-list simple test

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -2,12 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function createToDo(todayList) {
+    const getList = document.createElement("li");
+    const getSpan = document.createElement("span");
+    getSpan.innerText = todayList;
+    getList.appendChild(getSpan);
+    toDoList.appendChild(getList);
+}
 
 function toDoSubmit(event) {
     event.preventDefault(); 
     const newToDo = toDoInput.value;
     console.log(toDoInput.value);
     toDoInput.value = "";
+    createToDo(newToDo);
 };
 
 toDoForm.addEventListener("submit", toDoSubmit);
