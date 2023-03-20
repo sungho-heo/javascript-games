b9b15a4c35e95983bd3a3acc662543382bd62085
Sungho <hurgj123kr@gmail.com>
2023-03-20 09:30:33 +0900

    login name input next hello username

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,10 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const hello = document.querySelector("#user-hello");
+const CSS_HIDDEN = "hidden";
 
 
 function loginSubmit(event) {
     event.preventDefault(); // 방금 일어난 일에 정보를 담은 object임.
-    console.log(loginInput.value)
+    const userName = loginInput.value;
+    loginForm.classList.add(CSS_HIDDEN);
+    hello.innerText = "Hello " + userName;
+    hello.classList.remove(CSS_HIDDEN);
 };
 
 loginForm.addEventListener("submit", loginSubmit);
