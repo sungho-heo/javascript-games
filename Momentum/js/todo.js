0de39c7d35a6ea54f1a15027c178de092db8f0b3
Sungho <hurgj123kr@gmail.com>
2023-03-20 21:11:38 +0900

    todo-list create

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function offToDoList(event) {
+    const offli = event.target.parentElement;
+    offli.remove();
+}
+
 function createToDo(todayList) {
     const getList = document.createElement("li");
     const getSpan = document.createElement("span");
+    const buttonTodo = document.createElement("input");
+    buttonTodo.type = "checkbox";
+    buttonTodo.addEventListener("click", offToDoList);
     getSpan.innerText = todayList;
     getList.appendChild(getSpan);
+    getList.appendChild(buttonTodo);
     toDoList.appendChild(getList);
 }
 
