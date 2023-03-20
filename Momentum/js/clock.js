477bf4d261af581bf0dda86befcca55171629eb2
Sungho <hurgj123kr@gmail.com>
2023-03-20 13:22:13 +0900

    clock create

--- Momentum/js/clock.js
+++ Momentum/js/clock.js
@@ -1,7 +1,11 @@
 const clockKey = document.querySelector("h2#user-clock");
 
-function helloInterval() {
-    console.log("hello");
-}
-
-setInterval(helloInterval, 2000);
+function getDateInterval() {
+    const date = new Date();
+    clockKey.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    if (date.getSeconds() < 10) {
+        clockKey.innerText = `${date.getHours()}:${date.getMinutes()}:0${date.getSeconds()}`
+    }
+};
+getDateInterval();
+setInterval(getDateInterval, 1000);
