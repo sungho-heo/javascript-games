1b5c6fff55b684320f070607089110da946a947e
Sungho <hurgj123kr@gmail.com>
2023-03-23 10:18:41 +0900

    first get request create

--- hotube/src/server.js
+++ hotube/src/server.js
@@ -3,6 +3,10 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleGet = () => console.log("Sombody is try go home");
+
+app.get("/", handleGet);
+
 const handleAppListen = () => console.log(`good app:http://localhost:${PORT}/`)
 
 app.listen(PORT, handleAppListen);
