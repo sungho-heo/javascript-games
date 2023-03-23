52be0defaca7a0571a4572692df3a32d4b3a3d9d
Sungho <hurgj123kr@gmail.com>
2023-03-23 11:25:36 +0900

    res.send create

--- hotube/src/server.js
+++ hotube/src/server.js
@@ -3,9 +3,16 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleGet = () => console.log("Sombody is try go home");
+const handleGet = (req,res) => {
+    return res.send("good guy welcome");
+};
 
+const handleLogin = (req, res) => {
+    return res.send("welcome to login page");
+};
 app.get("/", handleGet);
+
+app.get("/login", handleLogin);
 
 const handleAppListen = () => console.log(`good app:http://localhost:${PORT}/`)
 
