175d207534f9fd6db119bdd7a59ba7f88b011672
Sungho <hurgj123kr@gmail.com>
2023-03-23 16:50:50 +0900

    global router and user router and video router create

--- hotube/src/server.js
+++ hotube/src/server.js
@@ -3,26 +3,33 @@ import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
+
 const loggerMorgan = morgan("dev");
 
-const handleGet = (req,res) => {
-    return res.status(404).send("Sorry, we cannot find that!");
+const globalRouter = express.Router();
 
+const handlerHome = (req, res, next) => {
+    return res.send("Home")
+}
+globalRouter.get("/", handlerHome);
+
+const userRouter = express.Router();
+const handlerEdit = (req, res, next) => {
+    return res.send("user-edit page");
 };
+userRouter.get("/edit", handlerEdit);
 
-// const logger = (req, res, next) => { me middlewares
-//     console.log(`${req.method} ${req.url}`);
-//     next()
-// };
-
-const handleLogin = (req, res) => {
-    return res.send("welcome to login page");
-};
+const videoRouter = express.Router();
+const handlerWatch = (req, res, next) => {
+    return res.send("watch page");
+}
+videoRouter.get("/watch", handlerWatch);
 
 // logger는 사용자가 어떤 페이지를 향하며 http method를 어떤걸 하는지 확인하기 위함.
 app.use(loggerMorgan);
-app.get("/", handleGet);
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 const handleAppListen = () => console.log(`good app:http://localhost:${PORT}/`)
 
