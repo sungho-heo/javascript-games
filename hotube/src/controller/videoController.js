66bf508ac0b97d990c79512872294afb459a4367
Sungho <hurgj123kr@gmail.com>
2023-03-25 14:32:14 +0900

    upload function add

--- hotube/src/controller/videoController.js
+++ hotube/src/controller/videoController.js
@@ -61,5 +61,15 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
+  const title = req.body.title;
+  const newVideo = {
+    title,
+    createBy: "who are you",
+    views: 1,
+    createdAt: "just now",
+    comments: 1,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
   return res.redirect("/");
 };
