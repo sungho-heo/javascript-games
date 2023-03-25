d1ff7f0a2e65735a525407ff60ddbfcfa33ba301
Sungho <hurgj123kr@gmail.com>
2023-03-25 20:48:53 +0900

    first mongodb data create

--- hotube/src/controller/videoController.js
+++ hotube/src/controller/videoController.js
@@ -1,9 +1,9 @@
 import Video from "../models/Video";
 
+// callback 방식을 사용하려는데 최신버전은 callback문법을 사용할수없어서 promise로 구현함.
 export const homeVideo = async (req, res) => {
   const video = await Video.find({});
-  console.log(video.error);
-    return res.render("home", { pageTitle: "Home" });
+  return res.render("home", { pageTitle: "Home" });
 };
 
 export const seeVideo = (req, res) => {
@@ -28,5 +28,20 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   const title = req.body.title;
+  const description = req.body.description;
+  const createdBy = req.body.createdBy;
+  const hashTag = req.body.hashTag;
+  const createVideo = new Video({
+    title: title,
+    description: description,
+    createdBy: createdBy,
+    createdAt: Date.now(),
+    hashTag: hashTag.split(",").map(word => `#${word}`),
+    meta: {
+      rating: 0,
+      views: 0,
+    },
+  });
+  console.log(createVideo);
   return res.redirect("/");
 };
