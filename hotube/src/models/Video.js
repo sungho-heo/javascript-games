2641aad41cfcd1f188d7f82e9bd694f78cb7dc2d
Sungho <hurgj123kr@gmail.com>
2023-03-26 14:45:18 +0900

    video view update and string data length limit add

--- hotube/src/models/Video.js
+++ hotube/src/models/Video.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 
 const videoSchema = new mongoose.Schema({
-    title: { type:String, required: true},
-    description: { type:String, required: true},
+    title: { type:String, required: true, maxlength: 30},
+    description: { type:String, required: true, minlength:30},
     createdBy: { type:String, required: true},
     createdAt: {type: Date, required: true, default: Date.now},
-    hashtags: [{ type: String }],
+    hashtags: [{ type: String, trim: true}],
     meta: { 
         rating: {type:Number,required: true,default:0},
         views: {type:Number, required: true,default:0},
