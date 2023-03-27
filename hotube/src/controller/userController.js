cf881ac84341540873e9b8905dda57208aecd48d
Sungho <hurgj123kr@gmail.com>
2023-03-27 14:26:03 +0900

    join function update save

--- hotube/src/controller/userController.js
+++ hotube/src/controller/userController.js
@@ -1,9 +1,23 @@
+import User from "../models/User.js";
+
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   console.log(req.body);
-  return res.render("join", { pageTitle: "Join" })
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const nickname = req.body.nickname;
+  const location = req.body.location;
+  await User.create({
+    email: email,
+    username: username,
+    password: password,
+    nickname: nickname,
+    location: location,
+  });
+  return res.redirect("/login");
 }
 export const edit = (req, res) => {
   return res.send("user-edit page");
