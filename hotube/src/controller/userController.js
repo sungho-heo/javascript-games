a0642be463bc5455d144f81132e3d15af7971444
Sungho <hurgj123kr@gmail.com>
2023-03-27 19:21:20 +0900

    uniqe db data validation create

--- hotube/src/controller/userController.js
+++ hotube/src/controller/userController.js
@@ -4,12 +4,28 @@ export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
 };
 export const postJoin = async (req, res) => {
-  console.log(req.body);
   const email = req.body.email;
   const username = req.body.username;
   const password = req.body.password;
+  const password2 = req.body.password2;
   const nickname = req.body.nickname;
   const location = req.body.location;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: pageTitle,
+      errorMessage: "Password confirmation does not match ",
+    });
+  };
+  const exists = User.exists({
+    $or: [{ email: email, username: username, nickname: nickname }],
+  });
+  if (exists) {
+    return res.render("join", {
+      pageTitle: pageTitle,
+      errorMessage: "The email/username/nickname alrady taken",
+    });
+  };
   await User.create({
     email: email,
     username: username,
