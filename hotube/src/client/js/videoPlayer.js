ae361c794d4cb29c7654f1e89c011de244d592ed
Sungho <hurgj123kr@gmail.com>
2023-04-02 14:32:15 +0900

    play and pause create

--- hotube/src/client/js/videoPlayer.js
+++ hotube/src/client/js/videoPlayer.js
@@ -1,1 +1,23 @@
-console.log("help");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayBtn = (e) => {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }  
+};
+
+const handleMuteBtn = (e) => { };
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+playBtn.addEventListener("click", handlePlayBtn);
+muteBtn.addEventListener("click", handleMuteBtn);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
