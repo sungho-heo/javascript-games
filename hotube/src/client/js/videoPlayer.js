322d236c4d5092abe0cde0c92c41931d17f03778
Sungho <hurgj123kr@gmail.com>
2023-04-02 20:05:38 +0900

    videoPlayer volume function create user volume save add

--- hotube/src/client/js/videoPlayer.js
+++ hotube/src/client/js/videoPlayer.js
@@ -4,7 +4,11 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 
-const handlePlayBtn = (e) => {
+let volumeValue = 0.5;
+video.volume = volumeValue;
+playBtn.innerText = "Play";
+
+const handlePlayBtn = (event) => {
     if (video.paused) {
         video.play();
     } else {
@@ -13,19 +17,31 @@ const handlePlayBtn = (e) => {
     playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handleMuteBtn = (e) => { 
+const handleMuteBtn = (event) => { 
     if (video.muted) {
         video.muted = false;
 
     } else {
         video.muted = true;
     }
-    muteBtn.innerText = video.muted ? "Mute" : "Unmute";
-    volume.value = video.muted ? 0 : 0.5;
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volume.value = video.muted ? 0 : volumeValue;
 };
 
+const handleVolumeChange = (event) => {
+    const value = event.target.value;
+    volumeValue = value;
+    video.volume = volumeValue;
+    if (video.volume===0) {
+        video.muted = true;
+    } else {
+        video.muted = false;
+    }
+    muteBtn.innerText = video.volume === 0 ? "Unmute" : "Mute";
+};
 
 playBtn.addEventListener("click", handlePlayBtn);
 muteBtn.addEventListener("click", handleMuteBtn);
+volume.addEventListener("input", handleVolumeChange);
 
 
