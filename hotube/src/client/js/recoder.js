d298eb9da830e11b2321b209a5b0c8fc6532a92a
Sungho <hurgj123kr@gmail.com>
2023-04-04 19:08:26 +0900

    video recorder create simple function

--- hotube/src/client/js/recoder.js
+++ hotube/src/client/js/recoder.js
@@ -1,14 +1,41 @@
 const recoderBtn = document.getElementById("recoderBtn");
 const video = document.getElementById("preview");
 
+let stream;
 
-const handleRecoderBtn = async() => {
-    const stream = await navigator.mediaDevices.getUserMedia({
+const handleStop = () => {
+    recoderBtn.innerText = "Start Recording";
+    recoderBtn.removeEventListener("click", handleStop);
+    recoderBtn.addEventListener("click", handleStart);
+}
+
+const handleStart = () => {
+    recoderBtn.innerText = "Stop Recording";
+    recoderBtn.removeEventListener("click", handleStart);
+    recoderBtn.addEventListener("click", handleStop);
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => {
+        console.log("recoder done");
+        console.log(event);
+        console.log(event.data);
+    };
+    console.log(recorder);
+    recorder.start();
+    console.log(recorder);
+    setTimeout(() => {
+        recorder.stop();
+    }, 10000);
+};
+
+const init = async() => {
+    stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true,
     });
     video.srcObject = stream;
     video.play();
 };
+
+init();
     
-recoderBtn.addEventListener("click", handleRecoderBtn);
+recoderBtn.addEventListener("click", handleStart);
