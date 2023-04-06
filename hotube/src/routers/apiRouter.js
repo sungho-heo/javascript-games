dbdba1bf1d0da0f22deaab5d2537a18d214bccee
Sungho <hurgj123kr@gmail.com>
2023-04-06 20:34:50 +0900

    createComment function create

--- hotube/src/routers/apiRouter.js
+++ hotube/src/routers/apiRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { pageViewCount } from "../controller/videoController";
+import { pageViewCount, createComment } from "../controller/videoController";
 
 const apiRouter = express.Router();
 
 
-apiRouter.post("/videos/:id([0-9a-f]{24})/view", pageViewCount)
+apiRouter.post("/videos/:id([0-9a-f]{24})/view", pageViewCount);
+apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
 
 
 export default apiRouter
